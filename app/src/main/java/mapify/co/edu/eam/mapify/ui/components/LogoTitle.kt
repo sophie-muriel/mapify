de9e593e3459b504a98fc3310adea79cb9c5630b
Sophie <miso.png@gmail.com>
2025-03-30 20:57:03 -0500

    Add typography, logo, and registration screen

---
+++ app/src/main/java/mapify/co/edu/eam/mapify/ui/components/LogoTitle.kt
@@ -0,0 +1,31 @@
+package mapify.co.edu.eam.mapify.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import co.edu.eam.mapify.R
+
+@Composable
+fun LogoTitle(aspectRatio: Float) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.mapify_transparent),
+            contentDescription = "Mapify Logo",
+            modifier = Modifier.aspectRatio(aspectRatio)
+        )
+
+        Text(
+            text = "Mapify",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
