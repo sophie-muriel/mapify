09ea8eda039ed592c49755a7ac3baa3457c4c3fb
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-03 01:59:11 -0500

    Add toBar elements

--- app/src/main/java/com/mapify/ui/screens/CreateReportScreen.kt
+++ app/src/main/java/com/mapify/ui/screens/CreateReportScreen.kt
@@ -1,9 +1,15 @@
 package com.mapify.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,6 +18,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,9 +46,17 @@ fun CreateReportScreen(){
                     }
                 },
                 actions = {
+                    IconButton(
+                        onClick = {  }
+                    )  {
+                        Icon(
+                            modifier = Modifier.size(48.dp),
+                            imageVector = Icons.Outlined.CheckCircle,
+                            contentDescription = "Check"
+                        )
+                    }
                 }
             )
-
         }
     ) { padding ->
 
