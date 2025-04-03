b496cc08c35b23f776fc9149e000fae97b3e44cc
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-03 02:42:22 -0500

    Create ReportCreationForm and add GenericTextField for title

--- app/src/main/java/com/mapify/ui/screens/CreateReportScreen.kt
+++ app/src/main/java/com/mapify/ui/screens/CreateReportScreen.kt
@@ -3,37 +3,67 @@ package com.mapify.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Lock
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.mapify.R
+import com.mapify.ui.components.GenericTextField
+import com.mapify.ui.theme.Spacing
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateReportScreen(){
 
+    var title by rememberSaveable { mutableStateOf("") }
+    var titleTouched by rememberSaveable { mutableStateOf(false) }
+    val titleError = titleTouched && title.isBlank()
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = "Create Report")
+                    Text(
+                        text = "Create Report",
+                        style = MaterialTheme.typography.titleLarge
+                    )
                 },
                 navigationIcon = {
                     IconButton(
@@ -60,9 +90,60 @@ fun CreateReportScreen(){
         }
     ) { padding ->
 
-        Column(modifier = Modifier.padding(padding)) {  }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            ReportCreationForm(
+                title = title,
+                onValueChangeTitle =  {
+                    title = it
+                    titleTouched = true
+                },
+                titleError = titleError
+            )
+        }
 
     }
 
+}
+
+@Composable
+fun ReportCreationForm(
+    title: String,
+    onValueChangeTitle: (String) -> Unit,
+    titleError: Boolean,
+    //email: String,
+    //onValueChangeEmail: (String) -> Unit,
+    //emailError: Boolean,
+    //password: String,
+    //onValueChangePassword: (String) -> Unit,
+    //passwordError: Boolean,
+    //passwordConfirmation: String,
+    //onValueChangePasswordConfirmation: (String) -> Unit,
+    //passwordConfirmationError: Boolean,
+    //onClickRegister: () -> Unit,
+    //navigateToLogin: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        GenericTextField(
+            value = title,
+            supportingText = "You must provide a title",
+            label = "Title",
+            onValueChange = onValueChangeTitle,
+            isError = titleError,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+
+
+    }
 }
 
