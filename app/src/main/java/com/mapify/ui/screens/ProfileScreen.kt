18836992a56184c229914d2efa2da2dd51b9bb99
Sophie <miso.png@gmail.com>
2025-04-04 01:41:21 -0500

    Optimize code, add colors, fix paddings and spacings, add toggle edit mode, unify topbar style, change dummy user variables to align with root user

--- app/src/main/java/com/mapify/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/mapify/ui/screens/ProfileScreen.kt
@@ -1,17 +1,23 @@
 package com.mapify.ui.screens
 
+import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material.icons.outlined.Lock
-import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.mapify.R
@@ -21,122 +27,154 @@ import com.mapify.ui.theme.Spacing
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
-    navigateBack: () -> Unit = {},
-    navigateToEditProfile: () -> Unit = {}
+    navigateToHome: () -> Unit
 ) {
+
+    val isKeyboardActive = WindowInsets.ime.getBottom(LocalDensity.current) > 0
+    var editMode by rememberSaveable { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = {
+            if (!isKeyboardActive) {
+                TopAppBar(modifier = Modifier.padding(horizontal = Spacing.Small), title = {
                     Text(
-                        text = stringResource(id = R.string.profile_screen_title),
+                        text = stringResource(id = R.string.profile_title),
                         style = MaterialTheme.typography.titleLarge
                     )
-                },
-                navigationIcon = {
-                    IconButton(onClick = navigateBack) {
+                }, navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navigateToHome()
+                        }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = stringResource(id = R.string.profile_back_icon_description)
+                            contentDescription = stringResource(id = R.string.back_arrow_icon)
                         )
                     }
-                },
-                actions = {
-                    IconButton(onClick = navigateToEditProfile) {
+                }, actions = {
+                    IconButton(
+                        onClick = { editMode = !editMode }) {
                         Icon(
-                            imageVector = Icons.Outlined.Settings,
-                            contentDescription = stringResource(id = R.string.profile_settings_icon_description)
+                            imageVector = if (editMode) Icons.Outlined.Close else Icons.Outlined.Edit,
+                            contentDescription = stringResource(
+                                id = if (editMode) R.string.close_icon_description else R.string.edit_icon_description
+                            )
                         )
                     }
-                }
-            )
-        }
-    ) { padding ->
+                })
+            }
+        }) { padding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padding)
-                .padding(horizontal = Spacing.Sides),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Spacer(modifier = Modifier.height(Spacing.TopBottomScreen / 2))
 
-            Icon(
-                imageVector = Icons.Outlined.Person,
-                contentDescription = stringResource(id = R.string.profile_avatar_icon_description),
-                modifier = Modifier
-                    .size(100.dp)
-                    .padding(bottom = 16.dp)
-            )
+            if (!editMode) {
+                ProfileInformation()
+            } else {
+                ProfileEdit()
+            }
 
-            Text(
-                text = stringResource(id = R.string.profile_greeting),
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-
-            Spacer(modifier = Modifier.height(Spacing.Inline * 2))
-
-            GenericTextField(
-                value = stringResource(id = R.string.profile_name_value),
-                label = stringResource(id = R.string.profile_name_label),
-                onValueChange = {},
-                isError = false,
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Person,
-                        contentDescription = stringResource(id = R.string.profile_name_icon_description)
-                    )
-                },
-                isSingleLine = true,
-                readOnly = true
-            )
-
-            GenericTextField(
-                value = stringResource(id = R.string.profile_email_value),
-                label = stringResource(id = R.string.profile_email_label),
-                onValueChange = {},
-                isError = false,
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Email,
-                        contentDescription = stringResource(id = R.string.profile_email_icon_description)
-                    )
-                },
-                isSingleLine = true,
-                readOnly = true
-            )
-
-            GenericTextField(
-                value = stringResource(id = R.string.profile_password_value),
-                label = stringResource(id = R.string.profile_password_label),
-                onValueChange = {},
-                isError = false,
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Lock,
-                        contentDescription = stringResource(id = R.string.profile_password_icon_description)
-                    )
-                },
-                isSingleLine = true,
-                isPassword = true,
-                readOnly = true
-            )
-
-            GenericTextField(
-                value = stringResource(id = R.string.profile_location_value),
-                label = stringResource(id = R.string.profile_location_label),
-                onValueChange = {},
-                isError = false,
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.LocationOn,
-                        contentDescription = stringResource(id = R.string.profile_location_icon_description)
-                    )
-                },
-                isSingleLine = true,
-                readOnly = true
-            )
+            Spacer(modifier = Modifier.height(Spacing.TopBottomScreen / 2))
         }
     }
+}
+
+@Composable
+fun ProfileInformation() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = Spacing.Sides),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.AccountCircle,
+            contentDescription = stringResource(id = R.string.profile_name_icon_description),
+            modifier = Modifier
+                .size(100.dp)
+                .padding(bottom = Spacing.Inline),
+            tint = MaterialTheme.colorScheme.primary
+        )
+
+        Text(
+            text = stringResource(id = R.string.profile_greeting),
+            style = MaterialTheme.typography.headlineSmall
+        )
+
+        Spacer(modifier = Modifier.height(Spacing.Inline * 2))
+
+        GenericTextField(
+            value = stringResource(id = R.string.profile_name_value),
+            label = stringResource(id = R.string.name_label),
+            onValueChange = {},
+            isError = false,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.AccountCircle,
+                    contentDescription = stringResource(id = R.string.profile_name_icon_description),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            },
+            isSingleLine = true,
+            readOnly = true
+        )
+
+        GenericTextField(
+            value = stringResource(id = R.string.profile_email_value),
+            label = stringResource(id = R.string.email_label),
+            onValueChange = {},
+            isError = false,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Email,
+                    contentDescription = stringResource(id = R.string.email_icon_description),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            },
+            isSingleLine = true,
+            readOnly = true
+        )
+
+        GenericTextField(
+            value = stringResource(id = R.string.profile_password_value),
+            label = stringResource(id = R.string.password_label),
+            onValueChange = {},
+            isError = false,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Lock,
+                    contentDescription = stringResource(id = R.string.password_icon_description),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            },
+            isSingleLine = true,
+            isPassword = true,
+            readOnly = true
+        )
+
+        GenericTextField(
+            value = stringResource(id = R.string.profile_location_value),
+            label = stringResource(id = R.string.location),
+            onValueChange = {},
+            isError = false,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.LocationOn,
+                    contentDescription = stringResource(id = R.string.profile_location_icon_description),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            },
+            isSingleLine = true,
+            readOnly = true
+        )
+    }
+}
+
+@Composable
+fun ProfileEdit() {
+
 }
