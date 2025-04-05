2691b63bf837087d64698bbd2126df75e990a4d3
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-05 00:17:43 -0500

    Add the coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.material.icons.extended)
+    implementation(libs.coil.compose)
 }
