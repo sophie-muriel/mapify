828202c811166b031e0ad37e76b25c9a79b79f48
Sophie <miso.png@gmail.com>
2025-04-06 17:34:03 -0500

    Modify and unify top bar

--- app/src/main/java/com/mapify/ui/components/SimpleTopBar.kt
+++ app/src/main/java/com/mapify/ui/components/SimpleTopBar.kt
@@ -1,8 +1,7 @@
 package com.mapify.ui.components
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -10,28 +9,49 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.mapify.R
-import com.mapify.ui.theme.Spacing
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SimpleTopBar(
+    contentAlignment: Alignment,
     text: String = "",
-    onClickArrowBack: () -> Unit
-){
-    TopAppBar(modifier = Modifier.padding(horizontal = Spacing.Small), title = {
-        Text(
-            text = text,
-            style = MaterialTheme.typography.titleLarge
-        )
-    }, navigationIcon = {
-        IconButton(onClick = onClickArrowBack) {
-            Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = stringResource(id = R.string.back_arrow_icon)
-            )
+    navIconVector: ImageVector,
+    navIconDescription: String,
+    onClickNavIcon: () -> Unit,
+    actions: Boolean,
+    actionIconVector: ImageVector? = null,
+    actionIconDescription: String = "",
+    onClickAction: () -> Unit = {},
+) {
+    TopAppBar(
+        title = {
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = contentAlignment) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = onClickNavIcon) {
+                Icon(
+                    imageVector = navIconVector,
+                    contentDescription = navIconDescription
+                )
+            }
+        },
+        actions = {
+            if (actions && actionIconVector != null) {
+                IconButton(onClick = onClickAction) {
+                    Icon(
+                        imageVector = actionIconVector,
+                        contentDescription = actionIconDescription,
+                    )
+                }
+            }
         }
-    })
+    )
 }
