196493f0d41d1a8037d8106415380ae0c86ab0c3
Sophie <miso.png@gmail.com>
2025-04-06 17:56:09 -0500

    Implement simple top bar in home screen

--- app/src/main/java/com/mapify/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mapify/ui/screens/HomeScreen.kt
@@ -5,21 +5,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.mapify.R
-import com.mapify.ui.theme.Spacing
 import com.mapify.ui.components.BottomNavigationBar
 import com.mapify.ui.components.CreateReportFloatingButton
+import com.mapify.ui.components.SimpleTopBar
 
 @Composable
 fun HomeScreen(
@@ -32,18 +28,27 @@ fun HomeScreen(
 ) {
     //TODO: add logout icon (convenient for tests, anyway)
 
-    Scaffold(topBar = {
-        TopNavigationBar(
-            onClickTop = {
-                navigateToProfile()
-            })
-    }, bottomBar = { BottomNavigationBar(homeSelected = true,
-        navigateToExplore = navigateToExplore,
-        navigateToNotifications = navigateToNotifications,
-        navigateToMessages = navigateToMessages) },
+    Scaffold(
+        topBar = {
+            SimpleTopBar(
+                Alignment.Center,
+                stringResource(id = R.string.app_name),
+                Icons.Filled.AccountCircle,
+                stringResource(id = R.string.name_icon_description),
+                { navigateToProfile() },
+                false
+            ) //TODO: add action for settings screen
+        }, bottomBar = {
+            BottomNavigationBar(
+                homeSelected = true,
+                navigateToExplore = navigateToExplore,
+                navigateToNotifications = navigateToNotifications,
+                navigateToMessages = navigateToMessages
+            )
+        },
         floatingActionButton = {
-        CreateReportFloatingButton(navigateToCreateReport = navigateToCreateReport)
-    }) { padding ->
+            CreateReportFloatingButton(navigateToCreateReport = navigateToCreateReport)
+        }) { padding ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -52,28 +57,4 @@ fun HomeScreen(
             Text(text = "Content", modifier = Modifier.padding(16.dp))
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopNavigationBar(
-    onClickTop: () -> Unit
-) {
-    TopAppBar(
-        modifier = Modifier.padding(horizontal = Spacing.Small),
-        title = {
-            Text(
-                text = stringResource(id = R.string.app_name),
-                style = MaterialTheme.typography.titleLarge
-            )
-        },
-        navigationIcon = {
-            IconButton(onClick = onClickTop) {
-                Icon(
-                    Icons.Filled.AccountCircle,
-                    contentDescription = stringResource(id = R.string.name_icon_description)
-                )
-            }
-        },
-    )
 }
