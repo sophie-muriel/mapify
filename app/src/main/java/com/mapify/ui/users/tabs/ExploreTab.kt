a257946c9e2b745a5395923f3fea8c9f1cfcb5cc
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-07 16:54:05 -0500

    Force report images in ExploreTab to be square

--- app/src/main/java/com/mapify/ui/users/tabs/ExploreTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/ExploreTab.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -26,6 +29,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -118,13 +122,18 @@ fun ReportCard(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(Spacing.Large),
         ) {
-            AsyncImage(
-                modifier = Modifier
+            Box(
+                modifier = Modifier.background(MaterialTheme.colorScheme.secondaryContainer)
                     .width(100.dp)
-                    .height(100.dp),
-                model = report.images[0],
-                contentDescription = stringResource(id = R.string.report_image)
-            )
+                    .height(100.dp)
+            ){
+                AsyncImage(
+                    modifier = Modifier.fillMaxSize(),
+                    model = report.images[0],
+                    contentDescription = stringResource(id = R.string.report_image),
+                    contentScale = ContentScale.FillBounds
+                )
+            }
 
             Column(
                 modifier = Modifier.weight(1f),
