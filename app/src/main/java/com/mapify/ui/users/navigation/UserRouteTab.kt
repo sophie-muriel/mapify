047b88d45554a2896763c5a30c3ec31e2501a015
Sophie <miso.png@gmail.com>
2025-04-06 20:19:58 -0500

    Optimize code

--- app/src/main/java/com/mapify/ui/users/navigation/UserRouteTab.kt
+++ app/src/main/java/com/mapify/ui/users/navigation/UserRouteTab.kt
@@ -4,15 +4,8 @@ import kotlinx.serialization.Serializable
 
 sealed class UserRouteTab {
 
-    @Serializable
-    data object Home: UserRouteTab()
-
-    @Serializable
-    data object Explore: UserRouteTab()
-
-    @Serializable
-    data object Notifications: UserRouteTab()
-
-    @Serializable
-    data object Messages: UserRouteTab()
+    @Serializable data object Home: UserRouteTab()
+    @Serializable data object Explore: UserRouteTab()
+    @Serializable data object Notifications: UserRouteTab()
+    @Serializable data object Messages: UserRouteTab()
 }
