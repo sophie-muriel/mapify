0233d30d5000bbb9ce5f8b523ec1e6548ae0b21a
Sophie <miso.png@gmail.com>
2025-04-06 20:19:25 -0500

    Refactor messages to tabs

--- app/src/main/java/com/mapify/ui/users/tabs/MessagesTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/MessagesTab.kt
@@ -1,9 +1,29 @@
 package com.mapify.ui.users.tabs
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.mapify.ui.components.MessageItem
+import com.mapify.ui.theme.Spacing
 
 @Composable
 fun MessagesTab(){
-    Text("Messages")
+    val dummyMessages = List(5) { "Lorem ipsum dolor sit amet, consectetur." }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = Spacing.Sides),
+        verticalArrangement = Arrangement.spacedBy(Spacing.Inline),
+    ) {
+        items(dummyMessages) { message ->
+            MessageItem(
+                sender = "List item",
+                message = message
+            )
+        }
+    }
 }
