8ca4237e30c45d281f7239d0a987119f3e4957fa
Sophie <miso.png@gmail.com>
2025-04-07 23:27:37 -0500

    Add message to know if user is admin or user

--- app/src/main/java/com/mapify/ui/users/tabs/HomeTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/HomeTab.kt
@@ -4,6 +4,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun HomeTab(){
-    Text("Home")
+fun HomeTab(
+    isAdmin: Boolean
+){
+    if (isAdmin) {
+        Text("Welcome, Admin! (｀・ω・´)ゞ")
+    } else {
+        Text("Welcome, User! (・∀・)")
+    }
 }
