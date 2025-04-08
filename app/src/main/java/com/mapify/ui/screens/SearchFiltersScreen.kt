ed497338ec842981511055c51e5abb2c34099cfe
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-08 12:58:23 -0500

    Create SwitchFilterRow composable to be reusable

--- app/src/main/java/com/mapify/ui/screens/SearchFiltersScreen.kt
+++ app/src/main/java/com/mapify/ui/screens/SearchFiltersScreen.kt
@@ -1,14 +1,19 @@
 package com.mapify.ui.screens
 
+import android.content.res.Resources.Theme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.CheckBox
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,6 +30,8 @@ import com.mapify.ui.components.SimpleTopBar
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.mapify.ui.theme.Spacing
 
@@ -34,7 +41,10 @@ fun SearchFiltersScreen(
 ) {
 
     var priorityChecked by rememberSaveable { mutableStateOf(false) }
-    var solvedChecked by rememberSaveable { mutableStateOf(false) }
+    var resolvedChecked by rememberSaveable { mutableStateOf(false) }
+    var verifiedChecked by rememberSaveable { mutableStateOf(false) }
+    var myPostsChecked by rememberSaveable { mutableStateOf(false) }
+
     //TODO: Navigation back to ExploreScreen with variables 
     Scaffold(
         topBar = {
@@ -57,8 +67,12 @@ fun SearchFiltersScreen(
            SearchFilters(
                priorityChecked = priorityChecked,
                onChangePriorityCheck = { priorityChecked = it },
-               solvedChecked = solvedChecked,
-               onChangeSolvedCheck = { solvedChecked = it}
+               resolvedChecked = resolvedChecked,
+               onChangeReSolvedCheck = { resolvedChecked = it },
+               myPostsChecked = myPostsChecked,
+               onChangeMyPostsCheck = { myPostsChecked = it },
+               verifiedChecked = verifiedChecked,
+               onChangeVerifiedChecked = { verifiedChecked = it }
            )
         }
     }
@@ -68,8 +82,58 @@ fun SearchFiltersScreen(
 fun SearchFilters(
     priorityChecked: Boolean,
     onChangePriorityCheck: (Boolean) -> Unit,
-    solvedChecked: Boolean,
-    onChangeSolvedCheck: (Boolean) -> Unit
+    resolvedChecked: Boolean,
+    onChangeReSolvedCheck: (Boolean) -> Unit,
+    myPostsChecked: Boolean,
+    onChangeMyPostsCheck: (Boolean) -> Unit,
+    verifiedChecked: Boolean,
+    onChangeVerifiedChecked: (Boolean) -> Unit
+){
+    SwitchFilterRow(
+        icon = Icons.Filled.Star,
+        description = stringResource(id = R.string.star_icon),
+        tint = MaterialTheme.colorScheme.primary,
+        stringResource(id = R.string.priority_filter),
+        isChecked = priorityChecked,
+        onChangeCheck = onChangePriorityCheck
+    )
+
+    SwitchFilterRow(
+        icon = Icons.Filled.Check,
+        description = stringResource(id = R.string.check_icon),
+        tint = MaterialTheme.colorScheme.secondary,
+        text = stringResource(id = R.string.resolved_filter),
+        isChecked = resolvedChecked,
+        onChangeCheck = onChangeReSolvedCheck
+    )
+
+    SwitchFilterRow(
+        icon = Icons.Filled.CheckBox,
+        description = stringResource(id = R.string.check_icon),
+        tint = MaterialTheme.colorScheme.primary,
+        text = stringResource(id = R.string.verified_filter),
+        isChecked = verifiedChecked,
+        onChangeCheck = onChangeVerifiedChecked
+    )
+
+    SwitchFilterRow(
+        icon = Icons.Filled.Person,
+        description = stringResource(id = R.string.person_icon),
+        tint = MaterialTheme.colorScheme.secondary,
+        text = stringResource(id = R.string.my_posts),
+        isChecked = myPostsChecked,
+        onChangeCheck = onChangeMyPostsCheck
+    )
+}
+
+@Composable
+fun SwitchFilterRow(
+    icon: ImageVector,
+    description: String,
+    tint: Color,
+    text: String,
+    isChecked: Boolean,
+    onChangeCheck: (Boolean) -> Unit
 ){
     Row(
         modifier = Modifier
@@ -82,48 +146,21 @@ fun SearchFilters(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
-                imageVector = Icons.Filled.Star,
-                contentDescription = stringResource(id = R.string.star_icon),
-                tint = MaterialTheme.colorScheme.primary
+                imageVector = icon,
+                contentDescription = description,
+                tint = tint
             )
 
             Text(
                 modifier = Modifier.padding(start = 12.dp),
-                text = stringResource(id = R.string.priority_filter)
+                text = text
             )
         }
 
         Switch(
-            checked = priorityChecked,
-            onCheckedChange = onChangePriorityCheck
+            checked = isChecked,
+            onCheckedChange = onChangeCheck
         )
     }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = Spacing.Sides, end = 48.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Row(
-            modifier = Modifier.weight(1f),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                imageVector = Icons.Filled.Check,
-                contentDescription = stringResource(id = R.string.check_icon),
-                tint = MaterialTheme.colorScheme.primary
-            )
-
-            Text(
-                modifier = Modifier.padding(start = 12.dp),
-                text = stringResource(id = R.string.resolved_filter)
-            )
-        }
-
-        Switch(
-            checked = solvedChecked,
-            onCheckedChange = onChangeSolvedCheck
-        )
-    }
+    Spacer(modifier = Modifier.height(Spacing.Inline))
 }
