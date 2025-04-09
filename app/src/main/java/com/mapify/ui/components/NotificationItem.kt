1ff6157c7e0d60eee77981a0238d31d944adb46e
Sophie <miso.png@gmail.com>
2025-04-09 00:50:21 -0500

    Modify notification item to fit style and be more useful in general

--- app/src/main/java/com/mapify/ui/components/NotificationItem.kt
+++ app/src/main/java/com/mapify/ui/components/NotificationItem.kt
@@ -1,5 +1,6 @@
 package com.mapify.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ElevatedCard
@@ -9,8 +10,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.mapify.R
 import com.mapify.ui.theme.Spacing
 
 @Composable
@@ -18,9 +27,9 @@ fun NotificationItem(
     title: String,
     status: String,
     supportingText: String,
-    statusColor: Color,
+    statusMessage: String,
     imageUrl: String,
-    reportDate: String,
+    statusColor: Color,
     onClick: () -> Unit
 ) {
     ElevatedCard(
@@ -30,51 +39,77 @@ fun NotificationItem(
             .clickable { onClick() }
     ) {
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(Spacing.Inline),
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(Spacing.Inline)
+            horizontalArrangement = Arrangement.spacedBy(Spacing.Large),
         ) {
-            AsyncImage(
-                model = imageUrl,
-                contentDescription = null,
+            Box(
                 modifier = Modifier
-                    .width(60.dp)
-                    .height(60.dp)
-            )
-
-            Column(
-                modifier = Modifier.weight(1f),
-                verticalArrangement = Arrangement.spacedBy(Spacing.Small)
+                    .background(MaterialTheme.colorScheme.secondaryContainer)
+                    .width(80.dp)
+                    .height(80.dp)
             ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
+                AsyncImage(
+                    modifier = Modifier.fillMaxSize(),
+                    model = imageUrl,
+                    contentDescription = stringResource(id = R.string.report_image),
+                    contentScale = ContentScale.FillBounds
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = Spacing.Small * 3),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(Spacing.Small),
                 ) {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                    Text(
-                        text = supportingText,
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.outline
-                    )
+                    Spacer(modifier = Modifier.height(Spacing.Small))
+
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(Spacing.Small)
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = title,
+                                style = MaterialTheme.typography.titleSmall,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                            Text(
+                                text = supportingText,
+                                style = MaterialTheme.typography.bodySmall
+                            )
+                        }
+
+                        Text(
+                            text = buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = statusColor,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                ) {
+                                    append(status)
+                                }
+                                append(" \u2022 ")
+                                append(statusMessage)
+                            },
+                            style = MaterialTheme.typography.bodySmall,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+
+                    }
                 }
-
-                Text(
-                    text = status,
-                    color = statusColor,
-                    style = MaterialTheme.typography.bodySmall
-                )
-
-                Text(
-                    text = reportDate,
-                    style = MaterialTheme.typography.bodySmall,
-                    color = MaterialTheme.colorScheme.outline
-                )
             }
         }
     }
