148016bcd882f0240bc1ad52a4f1aab4f00bd360
Sophie <miso.png@gmail.com>
2025-04-09 00:08:53 -0500

    Refactor messages screen to messages tab

--- app/src/main/java/com/mapify/ui/users/tabs/MessagesTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/MessagesTab.kt
@@ -3,30 +3,17 @@ package com.mapify.ui.users.tabs
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavHostController
-import com.mapify.R
 import com.mapify.model.Conversation
 import com.mapify.model.Message
-import com.mapify.ui.components.BottomNavigationBar
 import com.mapify.ui.components.MessageItem
-import com.mapify.ui.screens.formatMessageDate
 import com.mapify.ui.theme.Spacing
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MessagesTab(
     navigateToConversation: (Conversation) -> Unit
@@ -75,6 +62,26 @@ fun MessagesTab(
                     navigateToConversation(conversation)
                 }
             )
+        }
+    }
+}
+
+fun formatMessageDate(date: LocalDateTime): String {
+    val now = LocalDate.now()
+    val messageDate = date.toLocalDate()
+    return when {
+        messageDate.isEqual(now) -> {
+            val formatter = DateTimeFormatter.ofPattern("hh:mm a", Locale("es", "CO"))
+            date.format(formatter)
+        }
+
+        messageDate.isEqual(now.minusDays(1)) -> {
+            "Yesterday"
+        }
+
+        else -> {
+            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale("es", "CO"))
+            date.format(formatter)
         }
     }
 }
