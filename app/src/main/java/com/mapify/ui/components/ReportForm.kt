b605979cfb02060a47d8a3ecef6adf389d0c35aa
Sophie <miso.png@gmail.com>
2025-04-09 17:28:18 -0500

    Remove unused import

--- app/src/main/java/com/mapify/ui/components/ReportForm.kt
+++ app/src/main/java/com/mapify/ui/components/ReportForm.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
