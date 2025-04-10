735a966b66f91f0f67ef16aba4833540e0c7d740
Sophie <miso.png@gmail.com>
2025-04-10 01:14:34 -0500

    Add line so keyboard doesn't overflow top bar in manifest

--- app/src/main/java/com/mapify/MainActivity.kt
+++ app/src/main/java/com/mapify/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import com.mapify.ui.navigation.Navigation
 import com.mapify.ui.theme.MapifyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
