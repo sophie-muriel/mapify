8b488cadcd2fecddf9f884f67b79277e9b0b61aa
Sophie <miso.png@gmail.com>
2025-04-11 01:52:29 -0500

    Fix keyboard

--- app/src/main/java/com/mapify/ui/components/ReportForm.kt
+++ app/src/main/java/com/mapify/ui/components/ReportForm.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -76,7 +76,8 @@ fun ReportForm(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .padding(horizontal = Spacing.Sides),
+            .padding(horizontal = Spacing.Sides)
+            .navigationBarsPadding(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
