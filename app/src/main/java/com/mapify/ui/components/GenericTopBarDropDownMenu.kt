27cd85c63ec265198c5733bc9a931baccd376719
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-11 00:29:33 -0500

    Add strings to strings.xml and call them back

--- app/src/main/java/com/mapify/ui/components/GenericTopBarDropDownMenu.kt
+++ app/src/main/java/com/mapify/ui/components/GenericTopBarDropDownMenu.kt
@@ -18,6 +18,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.res.stringResource
+import com.mapify.R
 
 sealed class MenuAction(
     val label: String,
@@ -40,7 +42,7 @@ fun MinimalDropdownMenu(
     val expanded = remember { mutableStateOf(false) }
 
     IconButton(onClick = { expanded.value = !expanded.value }) {
-        Icon(Icons.Default.MoreVert, contentDescription = "More options")
+        Icon(Icons.Default.MoreVert, contentDescription = stringResource(id = R.string.more_options_icon))
     }
 
     val menuItems = getMenuItems(isAdmin, isCreator, isChat)
@@ -59,12 +61,13 @@ fun MinimalDropdownMenu(
     }
 }
 
+@Composable
 fun getMenuItems(isAdmin: Boolean?, isCreator: Boolean?, isChat: Boolean?): List<MenuAction> {
     return when {
         isChat == true -> listOf(
-            MenuAction.Simple("Mark as read", { Icon(Icons.Default.MarkChatRead, null) }, {}),
-            MenuAction.Simple("Mark as unread", { Icon(Icons.Default.MarkChatUnread, null) }, {}),
-            MenuAction.Simple("Delete", {
+            MenuAction.Simple(stringResource(id = R.string.mark_as_read), { Icon(Icons.Default.MarkChatRead, null) }, {}),
+            MenuAction.Simple(stringResource(id = R.string.mark_as_unread), { Icon(Icons.Default.MarkChatUnread, null) }, {}),
+            MenuAction.Simple(stringResource(id = R.string.delete), {
                 Icon(
                     Icons.Default.Delete,
                     contentDescription = null,
@@ -74,8 +77,8 @@ fun getMenuItems(isAdmin: Boolean?, isCreator: Boolean?, isChat: Boolean?): List
         )
 
         isCreator == true -> listOf(
-            MenuAction.Simple("Edit", { Icon(Icons.Default.Edit, null) }, {}),
-            MenuAction.Simple("Delete", {
+            MenuAction.Simple(stringResource(id = R.string.edit), { Icon(Icons.Default.Edit, null) }, {}),
+            MenuAction.Simple(stringResource(id = R.string.delete), {
                 Icon(
                     Icons.Default.Delete,
                     contentDescription = null,
@@ -85,9 +88,9 @@ fun getMenuItems(isAdmin: Boolean?, isCreator: Boolean?, isChat: Boolean?): List
         )
 
         isAdmin == true -> listOf(
-            MenuAction.Simple("Verify", { Icon(Icons.Default.CheckCircle, null) }, {}),
-            MenuAction.Simple("Reject", { Icon(Icons.Default.Unpublished, null) }, {}),
-            MenuAction.Simple("Delete", {
+            MenuAction.Simple(stringResource(id = R.string.verify), { Icon(Icons.Default.CheckCircle, null) }, {}),
+            MenuAction.Simple(stringResource(id = R.string.reject), { Icon(Icons.Default.Unpublished, null) }, {}),
+            MenuAction.Simple(stringResource(id = R.string.delete), {
                 Icon(
                     Icons.Default.Delete,
                     contentDescription = null,
@@ -97,7 +100,7 @@ fun getMenuItems(isAdmin: Boolean?, isCreator: Boolean?, isChat: Boolean?): List
         )
 
         else -> listOf(
-            MenuAction.Simple("Boost priority", {
+            MenuAction.Simple(stringResource(id = R.string.boost_priority), {
                 Icon(Icons.AutoMirrored.Filled.TrendingUp, null)
             }, {})
         )
