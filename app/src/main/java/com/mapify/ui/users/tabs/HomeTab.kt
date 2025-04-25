ecc84249ac7ff0b008007ac0990de1750a53e46b
Sophie <miso.png@gmail.com>
2025-04-24 20:36:20 -0500

    feat(home): add Mapbox map with markers and navigation to report detail

--- app/src/main/java/com/mapify/ui/users/tabs/HomeTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/HomeTab.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import com.mapbox.geojson.Point
 import com.mapbox.maps.extension.compose.MapboxMap
