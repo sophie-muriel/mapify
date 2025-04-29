da0601d9c70b5bbbfc46f1bc7f940df8dc533336
Sophie <miso.png@gmail.com>
2025-04-28 22:51:48 -0500

    feat(SharedPreferencesUtils):

---
+++ app/src/main/java/com/mapify/utils/SharedPreferencesUtils.kt
@@ -0,0 +1,39 @@
+package com.mapify.utils
+
+import android.content.Context
+import com.mapify.model.Role
+import androidx.core.content.edit
+
+object SharedPreferencesUtils {
+
+    fun savePreference(context: Context, userId: String, role: Role){
+        val sharedPreferences = context.getSharedPreferences("session", Context.MODE_PRIVATE)
+        sharedPreferences.edit() {
+            putString("userId", userId)
+            putString("role", role.toString())
+        }
+    }
+
+    fun clearPreference(context: Context){
+        val sharedPreferences = context.getSharedPreferences("session", Context.MODE_PRIVATE)
+        sharedPreferences.edit {
+            clear()
+        }
+    }
+
+    fun getPreference(context: Context): Map<String, String>{
+        val sharedPreferences = context.getSharedPreferences("session", Context.MODE_PRIVATE)
+        val userId = sharedPreferences.getString("userId", "")
+        val role = sharedPreferences.getString("role", "")
+
+        return if (userId.isNullOrEmpty() || role.isNullOrEmpty()){
+            emptyMap()
+        } else{
+            mapOf(
+                "userId" to userId,
+                "role" to role
+            )
+        }
+    }
+
+}
