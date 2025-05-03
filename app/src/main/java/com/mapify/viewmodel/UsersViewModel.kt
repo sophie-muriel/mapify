68b3166854f5f12fc1f1c520891d86a5de06e66a
Sophie <miso.png@gmail.com>
2025-05-03 18:51:13 -0500

    feat(UsersViewModel): add function to find a user by email, edit a user, get the currently logged in user, fix bug in delete function, and add more users to list of total users

--- app/src/main/java/com/mapify/viewmodel/UsersViewModel.kt
+++ app/src/main/java/com/mapify/viewmodel/UsersViewModel.kt
@@ -1,17 +1,26 @@
 package com.mapify.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.mapify.model.Location
 import com.mapify.model.Role
 import com.mapify.model.User
+import com.mapify.utils.SharedPreferencesUtils
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.UUID
 
 class UsersViewModel : ViewModel() {
 
     private val _users = MutableStateFlow(emptyList<User>())
     val users: StateFlow<List<User>> = _users.asStateFlow()
+    private val _currentUser = MutableStateFlow<User?>(null)
+    val currentUser: StateFlow<User?> = _currentUser
+
+    fun loadCurrentUser(userId: String) {
+        _currentUser.value = findById(userId)
+    }
 
     init {
         _users.value = getUsers()
@@ -21,12 +30,22 @@ class UsersViewModel : ViewModel() {
         _users.value += user
     }
 
+    fun edit(updatedUser: User, userId: String) {
+        _users.value = _users.value.map { user ->
+            if (user.id == userId) updatedUser else user
+        }
+    }
+
+    fun find(email: String): User? {
+        return _users.value.find { it.email == email }
+    }
+
     fun findById(userId: String): User? {
         return _users.value.find { it.id == userId }
     }
 
     fun delete(userId: String) {
-        _users.value -= users.value.filter { it.id != userId }
+        _users.value = _users.value.filter { it.id != userId }
     }
 
     fun login(email: String, password: String): User? {
@@ -36,7 +55,7 @@ class UsersViewModel : ViewModel() {
     private fun getUsers(): List<User> {
         return listOf(
             User(
-                id = "1",
+                id = UUID.randomUUID().toString(),
                 fullName = "Admin",
                 email = "root",
                 password = "root",
@@ -46,7 +65,7 @@ class UsersViewModel : ViewModel() {
                 )
             ),
             User(
-                id = "2",
+                id = UUID.randomUUID().toString(),
                 fullName = "Average User",
                 email = "user",
                 password = "user",
@@ -54,6 +73,51 @@ class UsersViewModel : ViewModel() {
                 registrationLocation = Location(
                     latitude = 43230.1, longitude = 753948.7, country = "Colombia", city = "Armenia"
                 )
+            ),
+            User(
+                id = UUID.randomUUID().toString(),
+                fullName = "Barry McCoquiner",
+                email = "barry.mccoquiner@example.com",
+                password = "pass1",
+                role = Role.CLIENT,
+                registrationLocation = Location(0.0, 0.0, "USA", "City"),
+                profileImageUrl = null
+            ),
+            User(
+                id = UUID.randomUUID().toString(),
+                fullName = "John Smith",
+                email = "john.smith@example.com",
+                password = "pass2",
+                role = Role.CLIENT,
+                registrationLocation = Location(0.0, 0.0, "USA", "City"),
+                profileImageUrl = null
+            ),
+            User(
+                id = UUID.randomUUID().toString(),
+                fullName = "Alice Johnson",
+                email = "alice.johnson@example.com",
+                password = "pass3",
+                role = Role.CLIENT,
+                registrationLocation = Location(0.0, 0.0, "USA", "City"),
+                profileImageUrl = null
+            ),
+            User(
+                id = UUID.randomUUID().toString(),
+                fullName = "Mike Cox",
+                email = "mike.cox@example.com",
+                password = "pass4",
+                role = Role.CLIENT,
+                registrationLocation = Location(0.0, 0.0, "USA", "City"),
+                profileImageUrl = null
+            ),
+            User(
+                id = UUID.randomUUID().toString(),
+                fullName = "Hugh Jass",
+                email = "hugh.jass@example.com",
+                password = "pass5",
+                role = Role.CLIENT,
+                registrationLocation = Location(0.0, 0.0, "USA", "City"),
+                profileImageUrl = null
             )
         )
     }
