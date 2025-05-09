841b582cef978dcc5b2d1019ca4de2678bdbc77b
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-09 18:41:47 -0500

    style(ConversationScreen): add some line spaces

--- app/src/main/java/com/mapify/ui/screens/ConversationScreen.kt
+++ app/src/main/java/com/mapify/ui/screens/ConversationScreen.kt
@@ -39,7 +39,7 @@ fun ConversationScreen(
 ) {
 
     val usersViewModel = LocalMainViewModel.current.usersViewModel
-    
+
     val allUsers by usersViewModel.users.collectAsState()
 
     var conversationsList by remember {
