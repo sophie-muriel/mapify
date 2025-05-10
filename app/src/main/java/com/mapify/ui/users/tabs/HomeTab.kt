ad3bad3bed5be26ef57d5cfa45a855b9ad37c15f
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-09 19:21:58 -0500

    feat(HomeTab): add reportsViewModel to acces stored reports

--- app/src/main/java/com/mapify/ui/users/tabs/HomeTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/HomeTab.kt
@@ -2,15 +2,24 @@ package com.mapify.ui.users.tabs
 
 import HandleLocationPermission
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.mapify.ui.components.Map
+import com.mapify.ui.navigation.LocalMainViewModel
 
 @Composable
 fun HomeTab(
     navigateToDetail: (String) -> Unit
 ) {
+    val reportsViewModel = LocalMainViewModel.current.reportsViewModel
+    val reports by reportsViewModel.reports.collectAsState()
+
     HandleLocationPermission(
         onPermissionGranted = {
-            Map(navigateToDetail = navigateToDetail)
+            Map(
+                navigateToDetail = navigateToDetail,
+                reports = reports
+            )
         }
     )
 }
