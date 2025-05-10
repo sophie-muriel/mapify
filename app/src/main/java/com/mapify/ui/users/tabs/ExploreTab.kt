9da8f3d73c51f37be51ec8490f4b17ca410bc4bd
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-09 19:31:27 -0500

    feat(ExploreTab): add reportsViewModel to acces stored reports

--- app/src/main/java/com/mapify/ui/users/tabs/ExploreTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/ExploreTab.kt
@@ -23,6 +23,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -40,6 +42,7 @@ import com.mapify.model.Location
 import com.mapify.model.Report
 import com.mapify.model.ReportStatus
 import com.mapify.ui.components.Map
+import com.mapify.ui.navigation.LocalMainViewModel
 import com.mapify.ui.theme.Spacing
 import java.time.LocalDateTime
 
@@ -47,49 +50,9 @@ import java.time.LocalDateTime
 fun ExploreTab(
     navigateToDetail: (String) -> Unit
 ) {
-    val storedReports = listOf(
-        Report(
-            id = "1",
-            title = "Report 1",
-            category = Category.SECURITY,
-            description = "This is a report",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkmoJWVhxab15KM_FQbk539hzwjN7qhyWeDw&s"),
-            location = Location(
-                latitude = 43230.1, longitude = 753948.7, country = "Colombia", city = "Armenia"
-            ),
-            status = ReportStatus.NOT_VERIFIED,
-            userId = "1",
-            date = LocalDateTime.now()
-        ),
-        Report(
-            id = "2",
-            title = "Report 2",
-            category = Category.PETS,
-            description = "This is an embedded test report to test the pets category and the resolved flag and verified status",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSHtshKCjboh0e9X3dP5l-igYWWA4C8-nSaw&s"),
-            location = Location(
-                latitude = 43230.1, longitude = 753948.7, country = "Colombia", city = "Armenia"
-            ),
-            status = ReportStatus.VERIFIED,
-            userId = "1",
-            date = LocalDateTime.now(),
-            isResolved = true,
-            priorityCounter = 25
-        ),
-        Report(
-            id = "3",
-            title = "Report 3",
-            category = Category.INFRASTRUCTURE,
-            description = "This is an embedded test report",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhAHUz_3weYlC2aCZNSsna_PNEqGHZ1Di0Eg&s"),
-            location = Location(
-                latitude = 43230.1, longitude = 753948.7, country = "Colombia", city = "Armenia"
-            ),
-            status = ReportStatus.VERIFIED,
-            userId = "2",
-            date = LocalDateTime.now()
-        ),
-    )
+
+    val reportsViewModel = LocalMainViewModel.current.reportsViewModel
+    val storedReports by reportsViewModel.reports.collectAsState()
 
     HandleLocationPermission(
         onPermissionGranted = {
