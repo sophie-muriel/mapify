49297aba318e81ebc293d92d64f059c171b67929
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-09 19:36:03 -0500

    feat(NotificationsTab): add reportsViewModel to acces stored reports

--- app/src/main/java/com/mapify/ui/users/tabs/NotificationsTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/NotificationsTab.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -16,6 +17,7 @@ import com.mapify.R
 import com.mapify.model.*
 import com.mapify.ui.components.GenericDialog
 import com.mapify.ui.components.NotificationItem
+import com.mapify.ui.navigation.LocalMainViewModel
 import com.mapify.ui.theme.Spacing
 import java.time.*
 import java.time.format.DateTimeFormatter
@@ -27,111 +29,9 @@ fun NotificationsTab(
 ) {
     var exitDialogVisible by rememberSaveable { mutableStateOf(false) }
 
-    val storedReports = listOf(
-        Report(
-            id = "1",
-            title = "Report 1",
-            category = Category.SECURITY,
-            description = "This is a report",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkmoJWVhxab15KM_FQbk539hzwjN7qhyWeDw&s"),
-            location = Location(43230.1, 753948.7, "Colombia", "Armenia"),
-            status = ReportStatus.PENDING_VERIFICATION,
-            userId = "1",
-            date = LocalDateTime.now(),
-            rejectionDate = LocalDateTime.now().minusDays(1),
-            isDeletedManually = true
-        ),
-        Report(
-            id = "2",
-            title = "Report 2",
-            category = Category.PETS,
-            description = "This is a test report...",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSHtshKCjboh0e9X3dP5l-igYWWA4C8-nSaw&s"),
-            location = Location(43230.1, 753948.7, "Colombia", "Armenia"),
-            status = ReportStatus.VERIFIED,
-            userId = "1",
-            date = LocalDateTime.now(),
-            isResolved = true,
-            priorityCounter = 25
-        ),
-        Report(
-            id = "3",
-            title = "Report 3",
-            category = Category.INFRASTRUCTURE,
-            description = "Another report example",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhAHUz_3weYlC2aCZNSsna_PNEqGHZ1Di0Eg&s"),
-            location = Location(43230.1, 753948.7, "Colombia", "Armenia"),
-            status = ReportStatus.VERIFIED,
-            userId = "2",
-            date = LocalDateTime.now().minusHours(13)
-        ),
-        Report(
-            id = "4",
-            title = "Report 4",
-            category = Category.COMMUNITY,
-            description = "Report about illegal dumping near the river.",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhAHUz_3weYlC2aCZNSsna_PNEqGHZ1Di0Eg&s"),
-            location = Location(43230.1, 753948.7, "Colombia", "Armenia"),
-            status = ReportStatus.VERIFIED,
-            userId = "3",
-            date = LocalDateTime.now().minusHours(3),
-            isResolved = false,
-            priorityCounter = 10
-        ),
-        Report(
-            id = "5",
-            title = "Report 5",
-            category = Category.SECURITY,
-            description = "Potholes causing problems in traffic.",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhAHUz_3weYlC2aCZNSsna_PNEqGHZ1Di0Eg&s"),
-            location = Location(43230.1, 753948.7, "Colombia", "Armenia"),
-            status = ReportStatus.PENDING_VERIFICATION,
-            userId = "4",
-            date = LocalDateTime.now().minusDays(1),
-            isResolved = false,
-            priorityCounter = 3,
-            rejectionDate = LocalDateTime.now().minusDays(2)
-        ),
-        Report(
-            id = "6",
-            title = "Report 6",
-            category = Category.PETS,
-            description = "Lost dog seen in the neighborhood.",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhAHUz_3weYlC2aCZNSsna_PNEqGHZ1Di0Eg&s"),
-            location = Location(43230.1, 753948.7, "Colombia", "Armenia"),
-            status = ReportStatus.VERIFIED,
-            userId = "5",
-            date = LocalDateTime.now().minusMinutes(45),
-            isResolved = true,
-            priorityCounter = 15,
-            isDeletedManually = true
-        ),
-        Report(
-            id = "1",
-            title = "Report 1",
-            category = Category.SECURITY,
-            description = "This is a report",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkmoJWVhxab15KM_FQbk539hzwjN7qhyWeDw&s"),
-            location = Location(43230.1, 753948.7, "Colombia", "Armenia"),
-            status = ReportStatus.PENDING_VERIFICATION,
-            userId = "1",
-            date = LocalDateTime.now(),
-            rejectionDate = LocalDateTime.now().minusDays(1)
-        ),
-        Report(
-            id = "2",
-            title = "Report 2",
-            category = Category.PETS,
-            description = "This is a test report...",
-            images = listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSHtshKCjboh0e9X3dP5l-igYWWA4C8-nSaw&s"),
-            location = Location(43230.1, 753948.7, "Colombia", "Armenia"),
-            status = ReportStatus.VERIFIED,
-            userId = "1",
-            date = LocalDateTime.now(),
-            isResolved = true,
-            priorityCounter = 25
-        )
-    )
+    val reportsViewModel = LocalMainViewModel.current.reportsViewModel
+
+    val storedReports by reportsViewModel.reports.collectAsState()
 
     var remainingDays = -1
 
