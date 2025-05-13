df5ec8a6c3b9bc53aa30eb5be0e4d2c3b9d23caf
Sophie <miso.png@gmail.com>
2025-05-13 10:58:02 -0500

    feat(Main): implement ConversationsViewModel in MainViewModel and MainActivity with appropriate handling for its parameter, UsersViewModel

--- app/src/main/java/com/mapify/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/mapify/viewmodel/MainViewModel.kt
@@ -3,5 +3,6 @@ package com.mapify.viewmodel
 class MainViewModel(
     val usersViewModel: UsersViewModel,
     val reportsViewModel: ReportsViewModel,
+    val conversationsViewModel: ConversationsViewModel
 ) {
 }
