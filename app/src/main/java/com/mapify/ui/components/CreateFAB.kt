f7139fe097136db739b22e0fe79a5d68066e038d
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-15 23:23:19 -0500

    refactor(CreateFAB): update button color handling to use primary by default and allow external override

--- app/src/main/java/com/mapify/ui/components/CreateFAB.kt
+++ app/src/main/java/com/mapify/ui/components/CreateFAB.kt
@@ -4,18 +4,21 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
 fun CreateFAB(
     onClick: () -> Unit,
     icon: ImageVector,
-    iconDescription: String
+    iconDescription: String,
+    color: Color = MaterialTheme.colorScheme.primary,
 ) {
 
     FloatingActionButton(
         onClick = onClick,
-        containerColor = MaterialTheme.colorScheme.primary
+        containerColor = color
     ) {
         Icon(
             imageVector = icon,
