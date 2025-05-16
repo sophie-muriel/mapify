345eac7719116ac8c1206dba0edfa739cd1ea299
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-16 16:14:08 -0500

    fix(Navigation): properly handle navigateBack after creating a new report

--- app/src/main/java/com/mapify/ui/navigation/Navigation.kt
+++ app/src/main/java/com/mapify/ui/navigation/Navigation.kt
@@ -132,6 +132,7 @@ fun Navigation(
                                 )
                             },
                             navigateToReportView = {
+                                navController.popBackStack()
                                 navController.navigate(RouteScreen.ReportView(it))
                             }
                         )
