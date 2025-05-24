77cfb4367d65bc81f8b0aba9e618bad0700305f5
Sophie <miso.png@gmail.com>
2025-05-23 22:58:16 -0500

    feat(conversations): modify Conversation and Message types to have defaults and QoL stuff

--- app/src/main/java/com/mapify/model/Message.kt
+++ app/src/main/java/com/mapify/model/Message.kt
@@ -3,9 +3,9 @@ package com.mapify.model
 import java.time.LocalDateTime
 
 data class Message(
-    val id: String,
-    val senderId: String,
-    val senderName: String,
-    val content: String,
+    val id: String = "",
+    val senderId: String = "",
+    val senderName: String = "",
+    val content: String = "",
     val timestamp: LocalDateTime
 )
