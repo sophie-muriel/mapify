3b3b06894f5c79f698b239246329c41e5ea9e954
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-27 18:27:53 -0500

    feat(RequestResult): add composable function RequestResultEffectHandler to handle request results

--- app/src/main/java/com/mapify/utils/RequestResult.kt
+++ app/src/main/java/com/mapify/utils/RequestResult.kt
@@ -1,7 +1,58 @@
 package com.mapify.utils
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import kotlinx.coroutines.delay
+
 sealed class RequestResult {
     data class Success(val message: String) : RequestResult()
     data class Failure(val message: String) : RequestResult()
     object Loading : RequestResult()
+}
+
+@Composable
+fun RequestResultEffectHandler(
+    requestResult: RequestResult?,
+    context: Context,
+    isLoading: MutableState<Boolean>,
+    isDeleting: MutableState<Boolean> = mutableStateOf(false),
+    onResetResult: () -> Unit,
+    onNavigateBack: () -> Unit = {},
+    needsUnitValue: Boolean = false
+) {
+    LaunchedEffect(key1 = if (needsUnitValue) Unit else requestResult) {
+        when (requestResult) {
+            null -> {
+                Log.d("ReportResult", "Result is null")
+                isLoading.value = false
+            }
+            is RequestResult.Success -> {
+                Log.d("ReportResult", "Success result detected")
+                isLoading.value = false
+                Toast.makeText(context, requestResult.message, Toast.LENGTH_SHORT).show()
+                delay(1000)
+                onResetResult()
+                if (isDeleting.value) {
+                    isDeleting.value = false
+                    onNavigateBack()
+                }
+            }
+            is RequestResult.Failure -> {
+                Log.d("ReportResult", "Failure result detected")
+                isLoading.value = false
+                Toast.makeText(context, requestResult.message, Toast.LENGTH_SHORT).show()
+                delay(1000)
+                onResetResult()
+            }
+            is RequestResult.Loading -> {
+                Log.d("ReportResult", "Loading result detected")
+                isLoading.value = true
+            }
+        }
+    }
 }
