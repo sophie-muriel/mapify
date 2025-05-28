d625463cdb313f848fe9b392e0f863f0f07f4e23
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-28 03:52:43 -0500

    feat(ReportFilters): add data class values

--- app/src/main/java/com/mapify/model/ReportFilters.kt
+++ app/src/main/java/com/mapify/model/ReportFilters.kt
@@ -1,3 +1,8 @@
-package com.mapify.utils
+package com.mapify.model
 
-data class ReportFilters()
+data class ReportFilters(
+    val onlyPriority: Boolean = false,
+    val onlyResolved: Boolean = false,
+    val onlyVerified: Boolean = false,
+    val onlyMyPosts: Boolean = false
+)
