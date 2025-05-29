24429ca9e5713350354ca21873319820b5f10448
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-29 00:55:13 -0500

    feat(ReportFilters): add new attributes to handle all filters

--- app/src/main/java/com/mapify/model/ReportFilters.kt
+++ app/src/main/java/com/mapify/model/ReportFilters.kt
@@ -4,5 +4,11 @@ data class ReportFilters(
     val onlyPriority: Boolean = false,
     val onlyResolved: Boolean = false,
     val onlyVerified: Boolean = false,
-    val onlyMyPosts: Boolean = false
+    val onlyMyPosts: Boolean = false,
+    val onlyThisDate: Boolean = false,
+    val onlyThisDistance: Boolean = false,
+    val thisDate: String = "",
+    val thisDistance: Double = 0.0,
+    val areSet: Boolean = onlyPriority || onlyResolved || onlyVerified || onlyMyPosts || onlyThisDate || onlyThisDistance || thisDate.isNotBlank() || thisDistance != 0.0,
+    val isJustDistance: Boolean = onlyThisDistance && thisDistance != 0.0 && !onlyPriority && !onlyResolved && !onlyVerified && !onlyMyPosts && !onlyThisDate
 )
