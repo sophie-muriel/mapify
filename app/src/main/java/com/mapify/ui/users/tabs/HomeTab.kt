11d7ee33916350af9319b0fd7104b5c6afa4a6d5
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-05-30 00:18:15 -0500

    feat(HomeTab): load reports to display and preload NotificationsTab's user filtered reports

--- app/src/main/java/com/mapify/ui/users/tabs/HomeTab.kt
+++ app/src/main/java/com/mapify/ui/users/tabs/HomeTab.kt
@@ -7,8 +7,13 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.LocalContext
 import com.mapify.ui.components.Map
 import com.mapify.ui.navigation.LocalMainViewModel
+import com.mapify.utils.RequestResultEffectHandler
+import com.mapify.utils.SharedPreferencesUtils
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
@@ -16,17 +21,23 @@ fun HomeTab(
     navigateToDetail: (String) -> Unit
 ) {
     val reportsViewModel = LocalMainViewModel.current.reportsViewModel
+    val context = LocalContext.current
+    val userId = SharedPreferencesUtils.getPreference(context)["userId"]
+    val reports by reportsViewModel.reports.collectAsState()
+    val reportRequestResult by reportsViewModel.reportRequestResult.collectAsState()
 
     LaunchedEffect(Unit) {
-        reportsViewModel.restartReportsRealtime()
-    }
-    DisposableEffect(Unit) {
-        onDispose {
-            reportsViewModel.resetReportsListener()
-        }
+        reportsViewModel.getReports()
+        reportsViewModel.getReportsByUserId(userId?: "")
     }
 
-    val reports by reportsViewModel.reports.collectAsState()
+    RequestResultEffectHandler(
+        requestResult = reportRequestResult,
+        context = context,
+        onResetResult = { reportsViewModel.resetReportRequestResult() },
+        onNavigate = {  },
+        showsMessage = false
+    )
 
     Map(
         navigateToDetail = navigateToDetail,
