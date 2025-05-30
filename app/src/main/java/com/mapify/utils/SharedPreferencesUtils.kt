76b6ed0df0ed1fdd703c27481961166cb0b19941
Sophie <miso.png@gmail.com>
2025-05-30 14:54:37 -0500

    style(sharedPreferencesUtils)

--- app/src/main/java/com/mapify/utils/SharedPreferencesUtils.kt
+++ app/src/main/java/com/mapify/utils/SharedPreferencesUtils.kt
@@ -6,7 +6,7 @@ import androidx.core.content.edit
 
 object SharedPreferencesUtils {
 
-    fun savePreference(context: Context, userId: String, role: Role){
+    fun savePreference(context: Context, userId: String, role: Role) {
         val sharedPreferences = context.getSharedPreferences("session", Context.MODE_PRIVATE)
         sharedPreferences.edit() {
             putString("userId", userId)
@@ -14,21 +14,21 @@ object SharedPreferencesUtils {
         }
     }
 
-    fun clearPreference(context: Context){
+    fun clearPreference(context: Context) {
         val sharedPreferences = context.getSharedPreferences("session", Context.MODE_PRIVATE)
         sharedPreferences.edit {
             clear()
         }
     }
 
-    fun getPreference(context: Context): Map<String, String>{
+    fun getPreference(context: Context): Map<String, String> {
         val sharedPreferences = context.getSharedPreferences("session", Context.MODE_PRIVATE)
         val userId = sharedPreferences.getString("userId", "")
         val role = sharedPreferences.getString("role", "")
 
-        return if (userId.isNullOrEmpty() || role.isNullOrEmpty()){
+        return if (userId.isNullOrEmpty() || role.isNullOrEmpty()) {
             emptyMap()
-        } else{
+        } else {
             mapOf(
                 "userId" to userId,
                 "role" to role
